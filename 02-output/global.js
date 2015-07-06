07909d34eeab3f7dde0146b296c7079a193d7acf
David Gardetti <dgardetti@gmail.com>
2015-07-06 11:14:56 -0500

    02 done

---
+++ 02-output/global.js
@@ -0,0 +1,7 @@
+alert("I am Iron Man!");
+console.log("I eat Wheaties");
+console.log("I don't got no cats.  I have dogs.");
+console.log("I eat at a restaurant.");
+console.log("I take like 20 naps a week.");
+console.log("I want to take 40 naps per week");
+alert("Later tater.");
