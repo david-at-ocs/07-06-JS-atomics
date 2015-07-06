51df2bc6cc011c2fc33054517bd9d34c717ab027
David Gardetti <dgardetti@gmail.com>
2015-07-06 15:38:31 -0500

    06B done

---
+++ 06-if-statements/06B-password-game/global.js
@@ -0,0 +1,10 @@
+var password = "kraftwerk"
+
+var passwordTry = prompt("Please enter your password");
+
+
+if (password == passwordTry) {
+    alert("Good Job");
+} else {
+    alert("sorry, the correct answer is " + password);
+}
