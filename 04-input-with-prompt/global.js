a0bcae426499d5706ce145a9603b079d38268578
David Gardetti <dgardetti@gmail.com>
2015-07-06 13:35:03 -0500

    04 done

--- 04-input-with-prompt/global.js
+++ 04-input-with-prompt/global.js
@@ -1,7 +1,14 @@
-var name = "Harry";
-var age = "101";
-var hair_color = "green";
+var userName = prompt("Please enter your name", "Please enter your name");
+var userAge = prompt("Please enter your age");
+var userHometown = prompt("Please enter your hometown");
+var userFavNum = prompt("Please enter your favorite number");
 
-console.log("my name is " + name);
-alert("my age is " + age);
-alert("I don't got no hair color. " + hair_color);
+console.log("Welcome " + userName);
+alert("You are " + userAge + " years old.");
+alert("You're from " + userHometown);
+console.log("Your favorite number is " + userFavNum);
+
+console.log(typeof userName);
+console.log(typeof userAge);
+console.log(typeof userHometown);
+console.log(typeof userFavNum);
