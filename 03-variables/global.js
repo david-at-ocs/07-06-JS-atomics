0947f2fde5283b871f94ad84647f00ee3d5669ec
David Gardetti <dgardetti@gmail.com>
2015-07-06 12:01:13 -0500

    03 Variables done

---
+++ 03-variables/global.js
@@ -0,0 +1,7 @@
+var name = "Harry";
+var age = "101";
+var hair_color = "green";
+
+console.log("my name is " + name);
+alert("my age is " + age);
+alert("I don't got no hair color. " + hair_color);
