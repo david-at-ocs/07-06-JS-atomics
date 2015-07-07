761953fd6a47183a904863e77449ad2fba30d649
David Gardetti <dgardetti@gmail.com>
2015-07-07 10:28:05 -0500

    07G done

---
+++ 07-while-loops/07G-fizzy-drinks/global.js
@@ -0,0 +1,8 @@
+
+for (var count = 99; count > 0; count--) {
+    if (count == 1) {
+      console.log(count + " bottle of beer on the wall...");
+    } else {
+      console.log(count + " bottles of beer on the wall...");
+    }
+}
