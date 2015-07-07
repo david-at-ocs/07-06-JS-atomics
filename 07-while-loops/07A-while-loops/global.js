e3bce21a4ea096d9ef4e853bac00f7c771a4534b
David Gardetti <dgardetti@gmail.com>
2015-07-06 22:07:59 -0500

    07A done

--- 07-while-loops/07A-while-loops/global.js
+++ 07-while-loops/07A-while-loops/global.js
@@ -1,27 +1,7 @@
-var side1 = prompt("Please enter a positive number for side 1:");
-var side2 = prompt("Please enter a positive number for side 2:");
-var side3 = prompt("Please enter a positive number for side 3:");
 
-side1 = parseInt(side1);
-side2 = parseInt(side2);
-side3 = parseInt(side3);
+var i = 0;
 
-console.log("side 1: " + side1);
-console.log("side 2: " + side2);
-console.log("side 3: " + side3);
-
-if ((side1 + side2) < side3) {
-  console.log(side1 + " + " + side2 + " < " + side3);
-  alert("This is not a valid triangle.");
-  
-} else if ((side1 + side3) < side2) {
-  console.log(side1 + " + " + side3 + " < " + side2);
-  alert("This is not a valid triangle.");
-  
-} else if ((side2 + side3) < side1) {
-  console.log(side2 + " + " + side3 + " < " + side1);
-  alert("This is not a valid triangle.");
-  
-} else {
-  alert("This is a triangle")
+while (i < 10) {
+    console.log(i);
+    i++;
 }
