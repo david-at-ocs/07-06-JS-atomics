ae7205b705f2c81b94b1c528499255204d7e9188
David Gardetti <dgardetti@gmail.com>
2015-07-06 22:37:30 -0500

    07C done

--- 07-while-loops/07C-fizzy-drink/global.js
+++ 07-while-loops/07C-fizzy-drink/global.js
@@ -1,18 +1,11 @@
-var start = prompt("start value");
-var end = prompt("ending value");
-var countby = prompt("count by");
-
-start = parseInt(start);
-end = parseInt(end);
-countby = parseInt(countby);
-
-while (end < start) {
-  end = prompt("enter an ending value greater than " + start);
-  end = parseInt(end);
-}
+var count = 99
 
 
-while (start < end) {
-    console.log(start);
-    start += countby;
+while (count > 0) {
+    if (count == 1) {
+      console.log(count + " bottle of beer on the wall...");
+    } else {
+      console.log(count + " bottles of beer on the wall...");
+    }
+    count--;
 }
